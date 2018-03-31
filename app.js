8de46c87b32e008834af32d172e2e657721cf480
nik <nikolay.tunin@rhinoda.com>
2018-03-31 15:17:36 +0600

    init

---
+++ app.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+}
+
+function onPauseButtonClick() {
+  alert("pause");
+}
+
+function onSoundButtonClick() {
+  alert("sound");
+}
+
+function onOnOffButtonClick() {
+  alert("on");
+}
+
+function onResetButtonClick() {
+  alert("reset");
+}
+
+function onUpButtonClick() {
+  alert("up");
+}
+
+function onDownButtonClick() {
+  alert("down");
+}
+
+function onLeftButtonClick() {
+  alert("left");
+}
+
+function onRightButtonClick() {
+  alert("right");
+}
+
+function onRotateButtonClick() {
+  alert("rotate");
+}
